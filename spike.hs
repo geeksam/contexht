425079c045aa092dca2aa1c68762a450577842ec
Sam Livingston-Gray <geeksam@gmail.com>
2009-06-04 22:36:39 -0700

    Refactored spike a bit

--- spike.hs
+++ spike.hs
@@ -1,53 +1,53 @@
 #!/usr/bin/env runhaskell
 
-import Contexht
+import Contexht hiding (specResults, runSpecs)
 
-specs = Context "A Thingy" [
-          Context "with a doohickey" [
-            Context "and a frobnitz" [
-              It "should do something" $ assert (True == False) "but it didn't",
-                                      -- ==> if False then PASS else FAIL "but it didn't"
-              It "should do another thing" PASS
-            ],
-            Pending "This hasn't been done yet" [
-              It "should prove P=NP" undefined
-            ]
-            -- DependentContext "if this works" (FAIL) [
-            --   It "should solve the halting problem" undefined
-            -- ]
-          ]
-        ]
+specs =
+  Context "A Thingy" [
+    Context "with a doohickey" [
+      Context "and a frobnitz" [
+        It "should do something" $ assert (True == False),
+                                -- ==> if False then PASS else FAIL "but it didn't"
+        It "should do another thing" PASS
+      ],
+      Pending "This hasn't been done yet" [
+        It "should prove P=NP" undefined
+      ]
+      -- DependentContext "if this works" (FAIL) [
+      --   It "should solve the halting problem" undefined
+      -- ]
+    ]
+  ]
 
 
 specResults     :: Spec -> String
-specResults spec = specBar spec ++
-                   "\n\n" ++ (specResultsIndented "" spec) ++
+specResults spec = "\n" ++ specBar spec ++
+                   "\n\n" ++ (specResultsIndented "      " spec) ++
                    "\n" ++ (specStatsDisplay spec)
-
-indentChildren               :: String -> (String -> Spec -> String) -> [Spec] -> String
-indentChildren indent f specs = concat $ map (f (indent ++ "  ")) specs
-
-
-specPendingIndented                            :: String -> Spec -> String
-specPendingIndented indent (It desc _)          = indent ++ "? " ++ desc ++ " (PENDING)\n"
-specPendingIndented indent (Pending desc specs) = indent ++ "? " ++ desc ++ "\n" ++ (indentChildren indent specPendingIndented specs)
-specPendingIndented indent (Context desc specs) = indent ++ "  " ++ desc ++ "\n" ++ (indentChildren indent specPendingIndented specs)
-
-
-specResultsIndented                            :: String -> Spec -> String
-specResultsIndented indent (It desc PASS)       = indent ++ "+ " ++ desc ++ " PASS\n"
-specResultsIndented indent (It desc (FAIL msg)) = indent ++ "! " ++ desc ++ " (FAIL: " ++ msg ++ ")\n"
-specResultsIndented indent (Pending desc specs) = indent ++ "? " ++ desc ++ "\n" ++ (indentChildren indent specPendingIndented specs)
-specResultsIndented indent (Context desc specs) = indent ++ "  " ++ desc ++ "\n" ++ (indentChildren indent specResultsIndented specs)
-
-
-specBar :: Spec -> String
-specBar (It desc PASS)       = "."
-specBar (It desc (FAIL msg)) = "X"
-specBar (Pending desc specs) = take (countPendingList specs) (repeat '?')
-specBar (Context desc specs) = childResults
   where
-    childResults = concat (map specBar specs)
+    overlay (a:as) (b:bs) = a : (overlay as bs)
+    overlay as [] = as
+    overlay [] bs = bs
 
-runSpecs = putStrLn . Main.specResults
-main = Main.runSpecs specs
+    indentChildren               :: String -> (String -> Spec -> String) -> [Spec] -> String
+    indentChildren indent f specs = concat $ map (f (indent ++ "  ")) specs
+
+    specLineIt      indent text glyph prefix       = (overlay prefix indent) ++ (overlay glyph "  ") ++ text ++ "\n"
+    specLineContext indent text specs = indent ++ "  " ++ text ++ "\n" ++ (indentChildren indent specResultsIndented specs)
+    specLinePending indent text specs = indent ++ "  " ++ text ++ "\n" ++ (indentChildren indent pendingSpecResults specs)
+
+    pendingSpecResults                            :: String -> Spec -> String
+    pendingSpecResults indent (It desc _)          = specLineIt      indent desc "-" "PEND"
+    pendingSpecResults indent (Context desc specs) = specLineContext indent desc specs
+    pendingSpecResults indent (Pending desc specs) = specLinePending indent desc specs
+
+    specResultsIndented                            :: String -> Spec -> String
+    specResultsIndented indent (It desc PASS)       = specLineIt      indent desc "-" ""
+    specResultsIndented indent (It desc FAIL)       = specLineIt      indent desc "-" "FAIL"
+    specResultsIndented indent (Context desc specs) = specLineContext indent desc specs
+    specResultsIndented indent (Pending desc specs) = specLinePending indent desc specs
+
+
+runSpecs = putStrLn
+         . specResults
+main = runSpecs specs
