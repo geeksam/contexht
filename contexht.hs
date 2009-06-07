6c606fd621684c36211d38657d3acab7aad094e2
Sam Livingston-Gray <geeksam@gmail.com>
2009-06-06 22:15:20 -0700

    Added spec display

--- contexht.hs
+++ contexht.hs
@@ -2,9 +2,9 @@ module Contexht where
   
 type SpecDescription = String
 
-data Spec = It SpecDescription SpecResult
-          | Context SpecDescription [Spec]
+data Spec = Context SpecDescription [Spec]
           | Pending SpecDescription [Spec]
+          | It SpecDescription SpecResult
           -- | DependentContext String SpecResult [Spec] -- "Dependent" because [Spec] may not run if SpecResult fails
           deriving (Show)
 
@@ -28,19 +28,19 @@ assertEqual x y = assert (x == y)
 
 -- Basic stat collection
 specStats                  :: Spec -> SpecStat
-specStats (It _ PASS)       = (1, 0, 0)
-specStats (It _ FAIL)       = (0, 1, 0)
+specStats (Context _ specs) = foldl1 addSpecStat $ map specStats specs
 specStats (Pending _ specs) = (0, 0, countPendingList specs)
-specStats (Context _ specs) = foldl1 addStat $ map specStats specs
-  where
-    addStat (a1, b1, c1) (a2, b2, c2) = (a1+a2, b1+b2, c1+c2)
+specStats (It _ FAIL)       = (0, 1, 0)
+specStats (It _ PASS)       = (1, 0, 0)
+
+addSpecStat (a1, b1, c1) (a2, b2, c2) = (a1+a2, b1+b2, c1+c2)
 
 
 -- Stat collection for Pending contexts:  every It nested under a Pending should be counted as a pending spec
 countPendingList specs             = sum (map countPendingSpec specs)
-countPendingSpec (It _ _)          = 1
 countPendingSpec (Context _ specs) = countPendingList specs
 countPendingSpec (Pending _ specs) = countPendingList specs
+countPendingSpec (It _ _)          = 1
 
 
 -- Stat summary display
@@ -59,18 +59,50 @@ specStatsDisplay spec = (specOrSpecs numSpecs) ++ " run.  " ++
 
 -- Red/green bar display
 specBar                     :: Spec -> String
+specBar (Context desc specs) = concat (map specBar specs)
+specBar (Pending desc specs) = take (countPendingList specs) (repeat '?')
 specBar (It desc PASS)       = "."
 specBar (It desc FAIL)       = "X"
-specBar (Pending desc specs) = take (countPendingList specs) (repeat '?')
-specBar (Context desc specs) = childResults
+
+
+-- Spec checklist display, with prefix to call out failing specs
+specChecklist n (Context desc specs)    = specLineContext n desc specs
+specChecklist n (Pending desc specs)    = specLinePending n desc specs
+specChecklist n (It desc PASS)          = specLineIt      n desc ""
+specChecklist n (It desc FAIL)          = specLineIt      n desc "FAIL"
+                                       
+pendingChecklist n (Context desc specs) = specLineContext n desc specs
+pendingChecklist n (Pending desc specs) = specLinePending n desc specs
+pendingChecklist n (It desc _)          = specLineIt      n desc "PEND"
+
+specLineContext n text specs = (prefix n) ++ text ++ (indentSublist n specChecklist specs)    where prefix n = cr ++ tab (n+1)
+specLinePending n text specs = (prefix n) ++ text ++ (indentSublist n pendingChecklist specs) where prefix n = cr ++ tab (n+1)
+specLineIt      n text alert = (prefix n) ++ text
   where
-    childResults = concat (map specBar specs)
+    prefix n = cr ++ (overlay alert $ tab (n+1)) ++ 
+                     (overlay "-"   $ tab 1)
+
+
+-- display helpers
+cr = "\n"
+tab n = take (n * tabSize) $ repeat ' '       where tabSize = 2
+
+indentSublist n checklist specs = concat $ map (checklist (n+1)) specs
+
+overlay (a:as) (b:bs) = a : (overlay as bs)
+overlay as [] = as
+overlay [] bs = bs
 
 
 -- Packaging all the various displays together...
 specResults     :: Spec -> String
-specResults spec = "\n" ++ (specStatsDisplay spec)
+specResults spec = cr ++ specBar spec ++
+                   cr ++ (specChecklist 2 spec) ++ cr ++
+                   cr ++ (specStatsDisplay spec)
+
 
 -- ...and running the above
-runSpecs = putStrLn . specResults
+runSpecs = putStrLn
+         . specResults
+
 
--- spike.hs
+++ spike.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env runhaskell
 
-import Contexht hiding (specResults, runSpecs)
+import Contexht
 
 specs =
   Context "A Thingy" [
@@ -11,7 +11,11 @@ specs =
         It "should do another thing" PASS
       ],
       Pending "This hasn't been done yet" [
-        It "should prove P=NP" undefined
+        It "should prove P=NP" undefined-- ,
+        --         Context "some other junk" [
+        --           It "foo" undefined,
+        --           It "bar" undefined
+        --         ]
       ]
       -- DependentContext "if this works" (FAIL) [
       --   It "should solve the halting problem" undefined
@@ -19,35 +23,4 @@ specs =
     ]
   ]
 
-
-specResults     :: Spec -> String
-specResults spec = "\n" ++ specBar spec ++
-                   "\n\n" ++ (specResultsIndented "      " spec) ++
-                   "\n" ++ (specStatsDisplay spec)
-  where
-    overlay (a:as) (b:bs) = a : (overlay as bs)
-    overlay as [] = as
-    overlay [] bs = bs
-
-    indentChildren               :: String -> (String -> Spec -> String) -> [Spec] -> String
-    indentChildren indent f specs = concat $ map (f (indent ++ "  ")) specs
-
-    specLineIt      indent text glyph prefix       = (overlay prefix indent) ++ (overlay glyph "  ") ++ text ++ "\n"
-    specLineContext indent text specs = indent ++ "  " ++ text ++ "\n" ++ (indentChildren indent specResultsIndented specs)
-    specLinePending indent text specs = indent ++ "  " ++ text ++ "\n" ++ (indentChildren indent pendingSpecResults specs)
-
-    pendingSpecResults                            :: String -> Spec -> String
-    pendingSpecResults indent (It desc _)          = specLineIt      indent desc "-" "PEND"
-    pendingSpecResults indent (Context desc specs) = specLineContext indent desc specs
-    pendingSpecResults indent (Pending desc specs) = specLinePending indent desc specs
-
-    specResultsIndented                            :: String -> Spec -> String
-    specResultsIndented indent (It desc PASS)       = specLineIt      indent desc "-" ""
-    specResultsIndented indent (It desc FAIL)       = specLineIt      indent desc "-" "FAIL"
-    specResultsIndented indent (Context desc specs) = specLineContext indent desc specs
-    specResultsIndented indent (Pending desc specs) = specLinePending indent desc specs
-
-
-runSpecs = putStrLn
-         . specResults
 main = runSpecs specs
